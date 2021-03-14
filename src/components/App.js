28cfff99d4471af6d2a481f873357f526f3fdbfa
venkatmithun <venkatmithun.k@gmail.com>
2021-03-14 06:28:46 -0700

    added base name:

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import '../styles/App.css';
 import React from 'react';
-import { Router, Redirect, Route, Switch } from 'react-router-dom';
+import {
+  Router,
+  Redirect,
+  Route,
+  Switch,
+  BrowserRouter,
+} from 'react-router-dom';
 import { Navigation } from './Navigation';
 import Home from './home/Home';
 import Login from './login/Login';
@@ -16,7 +22,7 @@ class App extends React.Component {
   }
   render() {
     return (
-      <Router history={history}>
+      <BrowserRouter basename="/home" history={history}>
         <Navigation />
         <Switch>
           <Route exact path="/">
@@ -27,7 +33,7 @@ class App extends React.Component {
           <Route path="/signup" component={Signup} />
           {/* <PrivateRoute authed={this.state.authed} path='/dashboard' component={Dashboard} /> */}
         </Switch>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
